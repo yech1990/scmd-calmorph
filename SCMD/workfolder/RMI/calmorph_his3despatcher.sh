78e261f2692094222ab461e37827d0067b2ee205
matsumiya <matsumiya@localhost>
2005-08-25 01:50:33 +0000

    ???

--- SCMD/workfolder/RMI/calmorph_his3despatcher.sh
+++
@@ -1,6 +0,0 @@
-#!/export/home/sesejun/bin/bin_ia32/zsh
-
-for i in {01..14};do 
-    ssh cb$i '/export/home/sesejun/work/scmd/data/his3/calmorph_his3client.sh' &
-    ssh cb$i '/export/home/sesejun/work/scmd/data/his3/calmorph_his3client.sh' &
-done
