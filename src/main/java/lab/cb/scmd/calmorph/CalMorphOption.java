e841e39cc67e77caee20fb4c02d38e8bd0290224
yechang <yech1990@gmail.com>
2019-12-15 19:24:37 +0800

    - add options (calA, calD, outimage) for CUI

--- src/main/java/lab/cb/scmd/calmorph/CalMorphOption.java
+++ src/main/java/lab/cb/scmd/calmorph/CalMorphOption.java
@@ -10,76 +10,97 @@
 package lab.cb.scmd.calmorph;
 
 /**
- * This class holds the information required to run CalMorph, 
+ * This class holds the information required to run CalMorph,
  * e.g. output directory, input photo directory, etc.
  *
  * @author leo
- *
  */
-public class CalMorphOption
-{
-	String orfName;
+public class CalMorphOption {
+    String orfName;
     String outputDirectory;
     String inputDirectory;
     String xmlOutputDirectory;
     int maxImageNumber = 0;
-    
-    public CalMorphOption()
-    {
-        
+    private boolean calD = true;
+    private boolean calA = false;
+    private boolean outimage = true;
+
+    public CalMorphOption() {
+
     }
 
-    public String getInputDirectory()
-    {
-        return inputDirectory;
-    }
-
-    public void setInputDirectory(String inputDirectory)
-    {
+    // inputDirectory
+    public void setInputDirectory(String inputDirectory) {
         this.inputDirectory = inputDirectory;
     }
 
-    public String getOutputDirectory()
-    {
-        return outputDirectory;
+    public String getInputDirectory() {
+        return inputDirectory;
     }
 
-    public void setOutputDirectory(String outputDirectory)
-    {
+    // outputDirectory
+    public void setOutputDirectory(String outputDirectory) {
         this.outputDirectory = outputDirectory;
     }
 
-    public String getXmlOutputDirectory()
-    {
-        return xmlOutputDirectory;
+    public String getOutputDirectory() {
+        return outputDirectory;
     }
 
-    public void setXmlOutputDirectory(String xmlOutputDirectory)
-    {
+    // xmlOutputDirectory
+    public void setXmlOutputDirectory(String xmlOutputDirectory) {
         this.xmlOutputDirectory = xmlOutputDirectory;
     }
 
-    public int getMaxImageNumber()
-    {
+    public String getXmlOutputDirectory() {
+        return xmlOutputDirectory;
+    }
+
+    // maxImageNumber
+    public void setMaxImageNumber(int maxImageNumber) {
+        this.maxImageNumber = maxImageNumber;
+    }
+
+    public int getMaxImageNumber() {
         return maxImageNumber;
     }
 
-    
-    public String getOrfName() {
-		return orfName;
-	}
 
-	public void setOrfName(String orfName) {
-		this.orfName = orfName;
-	}
-
-	public void setMaxImageNumber(int maxImageNumber)
-    {
-        this.maxImageNumber = maxImageNumber;
+    // orfName
+    public void setOrfName(String orfName) {
+        this.orfName = orfName;
     }
-    
-    
-    
+
+    public String getOrfName() {
+        return orfName;
+    }
+
+    // calD
+    public void setCalD(boolean calD) {
+        this.calD = calD;
+    }
+
+    public boolean getCalD() {
+        return calD;
+    }
+
+    // calA
+    public void setCalA(boolean calA) {
+        this.calA = calA;
+    }
+
+    public boolean getCalA() {
+        return calA;
+    }
+
+    // outimage
+    public void setOutimage(boolean outimage) {
+        this.outimage = outimage;
+    }
+
+    public boolean getOutimage() {
+        return outimage;
+    }
 }
 
 
